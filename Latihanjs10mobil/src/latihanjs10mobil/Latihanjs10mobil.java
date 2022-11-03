484be22f1ddf44a80ab53f1ef647014ac996f3db
RizkyNafarelSidiq <112645159+RizkyNafarelSidiq@users.noreply.github.com>
2022-11-03 19:42:24 +0700

    Add files via upload
    Nama: Rizky Nafarel Sidiq
    Nim: 21343071
    Pratikum Pemograman Berorientasi Objek

---
+++ Latihanjs10mobil/src/latihanjs10mobil/Latihanjs10mobil.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihanjs10mobil;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Latihanjs10mobil {
+        String merk, warna;
+        int tahunproduksi;
+        
+        public void maju(){
+            int kec = 10;
+            System.out.println("Mobil majau sejauh " + kec + " meter");
+    }
+        public void mundur (){
+            int kec = 5;
+            System.out.println("Mobil mundur sejauh " + kec + " meter");
+        }
+    
+}
---
+++ Latihanjs10mobil/src/latihanjs10mobil/Mahasiswajs10.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanjs10mobil;
+
+/**
+ *
+ * // Created Rizky Nafarel Sidiq 21343071
+ */
+public class Mahasiswajs10 {
+    String nama, jur, univ;
+	
+	int nim;       
+}
